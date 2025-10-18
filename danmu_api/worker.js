b77fd1af006d0964ea679393958f18de39bca94d
linsky123 <13400636895@163.com>
2025-10-18 17:32:48 +0800

    Update worker.js

--- danmu_api/worker.js
+++ danmu_api/worker.js
@@ -43,7 +43,7 @@ function resolveOtherServer(env) {
   return DEFAULT_OTHER_SERVER;
 }
 
-const DEFAULT_VOD_SERVERS = "vod@https://www.caiji.cyou,听风@https://gctf.tfdh.top"; // 默认 vod站点配置，格式：名称@URL,名称@URL
+const DEFAULT_VOD_SERVERS = "vod@https://www.caiji.cyou,vod2@https://gctf.tfdh.top"; // 默认 vod站点配置，格式：名称@URL,名称@URL
 let vodServers = [];
 
 function resolveVodServers(env) {
